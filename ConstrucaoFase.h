4a7610c755919ca2794a46a062059670132b3675
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2019-10-04 16:56:07 -0300

    Random movement (#8)
    * basic of random movement ready
    
    * resolves the stack problem

--- ConstrucaoFase.h
+++ ConstrucaoFase.h
@@ -9,16 +9,72 @@
 
 using namespace std;
 
-const int tamanhoMatrizFase = 72, tamanhoVetorObjetos = (tamanhoMatrizFase - 2) * (tamanhoMatrizFase - 2);
+const int tamanhoMatrizFase = 62, tamanhoVetorObjetos = (tamanhoMatrizFase - 2) * (tamanhoMatrizFase - 2);
 
 
+void clearScreen(HANDLE hConsole){
+	COORD coordScreen = { 0, 0 };    // home for the cursor 
+	DWORD cCharsWritten;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD dwConSize;
 
+	// Get the number of character cells in the current buffer. 
 
-void ZerarMatrizFase(char matrizFase[tamanhoMatrizFase][2 * tamanhoMatrizFase], int tamanhoFase){
-	for (int i = 0; i < tamanhoFase; i++){
-		for (int j = 0; j < 2 * tamanhoFase; j++){
-			matrizFase[i][j] = 'a';
-		}
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)){
+		return;
+	}
+
+	dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+
+	// Fill the entire screen with blanks.
+
+	if (!FillConsoleOutputCharacter(hConsole,        // Handle to console screen buffer 
+		(TCHAR) ' ',     // Character to write to the buffer
+		dwConSize,       // Number of cells to write 
+		coordScreen,     // Coordinates of first cell 
+		&cCharsWritten))// Receive number of characters written
+	{
+		return;
+	}
+
+	// Get the current text attribute.
+
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)){
+		return;
+	}
+
+	// Set the buffer's attributes accordingly.
+
+	if (!FillConsoleOutputAttribute(hConsole,         // Handle to console screen buffer 
+		csbi.wAttributes, // Character attributes to use
+		dwConSize,        // Number of cells to set attribute 
+		coordScreen,      // Coordinates of first cell 
+		&cCharsWritten)) // Receive number of characters written
+	{
+		return;
+	}
+
+	// Put the cursor at its home coordinates.
+
+	SetConsoleCursorPosition(hConsole, coordScreen);
+}
+
+void cls(){
+	HANDLE hStdout;
+
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	clearScreen(hStdout);
+}
+
+void ZerarMatrizFase(char matrizFase[tamanhoMatrizFase][2 * tamanhoMatrizFase], int tamanhoFase, int i = 0, int j = 0){
+		
+		matrizFase[i][j] = 'a';
+	if (j < tamanhoFase * 2 - 1){
+		ZerarMatrizFase(matrizFase, tamanhoFase, i, j + 1);
+	}
+	else if (i < tamanhoFase - 1){
+		ZerarMatrizFase(matrizFase, tamanhoFase, i + 1, 0);
 	}
 }
 
@@ -28,14 +84,14 @@ bool teste(int valor, int array[], int max){
 		return false;
 }
 
-void geraNumeroAleatorio(int array[], int tamanhoObjetos){
+void geraNumeroAleatorio(int array[], int tamanhoArray){
 	int i = 0, x = 0;
 
-	memset(array, 0, sizeof(int) * tamanhoObjetos);
+	memset(array, 0, sizeof(int) * tamanhoArray);
 	srand(time(NULL));
 	while(true){
-		if (i == tamanhoObjetos) break;
-		x = (1 + (rand() % tamanhoObjetos));
+		if (i == tamanhoArray) break;
+		x = (1 + (rand() % tamanhoArray));
 		if(!teste(x, array, i)) array[i++] = x;
 	}
 }
@@ -86,12 +142,15 @@ void criacaoFase(char matrizFase[tamanhoMatrizFase][2 * tamanhoMatrizFase], int 
 	}
 }
 
-void exibeFase(char matrizFase[tamanhoMatrizFase][2 * tamanhoMatrizFase], int tamanhoFase) {
-	for (int i = 0; i < tamanhoFase; i++) {
-		for (int j = 0; j < tamanhoFase * 2; j++) {
-			cout << matrizFase[i][j];
-		}
+void exibeFase(char matrizFase[tamanhoMatrizFase][2 * tamanhoMatrizFase], int tamanhoFase, int i = 0, int j = 0) {
+	
+		cout << matrizFase[i][j];
+	if (j < tamanhoFase * 2 - 1){
+		exibeFase(matrizFase, tamanhoFase, i, j + 1);
+	}
+	else if (i < tamanhoFase - 1){
 		cout << endl;
+		exibeFase(matrizFase, tamanhoFase, i + 1, 0);
 	}
 }
 
--- Main.cpp
+++ Main.cpp
@@ -2,28 +2,47 @@
 
 
 
+void transformarVetorObjetoEmMatriz(int vetorDosObjetos[], int matrizObjetos[tamanhoMatrizFase][tamanhoMatrizFase], int tamanhoFase, int tamanhoObjetos){
+	for (int i = 0, k = 0; i < tamanhoFase; i++){
+		for (int j = 0; j < tamanhoFase; j++){
+			if (i == 0 || i == tamanhoFase - 1 || j == 0 || j == tamanhoFase - 1){
+				matrizObjetos[i][j] = tamanhoObjetos;
+			} else {
+				matrizObjetos[i][j] = vetorDosObjetos[k];
+				k++;
+			}
+		}
+	}
+}
+
+
 void main() {
 	int x = 0;
 	char matrizFase[tamanhoMatrizFase][2 * tamanhoMatrizFase];
+	int matrizObjetos[tamanhoMatrizFase][tamanhoMatrizFase];
 	int vetorObjetos[tamanhoVetorObjetos];
+	int valorPosicaoRato[2], valorPosicaoQueijo[2];
 	int tamanhoFase = 7;
 	int tamanhoObjetos = (tamanhoFase - 2) * (tamanhoFase - 2);
-	int quantParedes = tamanhoObjetos - tamanhoObjetos * 9 /25;
-	
+	int inicioParedes = tamanhoObjetos - tamanhoObjetos * 9 /25;
+
 	while (x != 3){
 		do{	
-			cout << "digite o tamanho desejado da fase!(Ex: 5 para criar uma fase 5x5, tendo como limite valores entre 2 e 70)\n-";
+			cout << "digite o tamanho desejado da fase!(Ex: 5 para criar uma fase 5x5, tendo como limite valores entre 2 e 60)\n-";
 			cin >> tamanhoFase;
-			tamanhoFase += 2, tamanhoObjetos = (tamanhoFase - 2) * (tamanhoFase - 2), quantParedes = tamanhoObjetos - tamanhoObjetos * 9 /25, x = 0;
+			tamanhoFase += 2, tamanhoObjetos = (tamanhoFase - 2) * (tamanhoFase - 2), inicioParedes = tamanhoObjetos - tamanhoObjetos * 9 /25, x = 0;
 			system("cls");
-		}while (!(tamanhoFase >= 4 && tamanhoFase <= 72));
+		}while (!(tamanhoFase >= 4 && tamanhoFase <= 62));
 		while (x != 2 && x != 3) {
-			criacaoFase(matrizFase, vetorObjetos, tamanhoFase, tamanhoObjetos, quantParedes);
+			criacaoFase(matrizFase, vetorObjetos, tamanhoFase, tamanhoObjetos, inicioParedes);
 			exibeFase(matrizFase, tamanhoFase);
-			if (!validacaoDaFase(vetorObjetos, tamanhoFase, tamanhoObjetos, quantParedes)){
+			transformarVetorObjetoEmMatriz(vetorObjetos, matrizObjetos, tamanhoFase, tamanhoObjetos);
+			if (!validacaoDaFase(matrizObjetos, tamanhoFase, tamanhoObjetos, inicioParedes, valorPosicaoRato, valorPosicaoQueijo)){
 				cout << "Hoje nao tem queijo!!\n";
 			} else {
 				cout << "Fase validada!!\n";
+				movimentoAleatorioRato(matrizObjetos, matrizFase, tamanhoFase, tamanhoObjetos, valorPosicaoRato[0], valorPosicaoRato[1], valorPosicaoQueijo[0], valorPosicaoQueijo[1], inicioParedes);
+				//movimentoInteligenteRato(matrizObjetos, matrizFase, tamanhoFase, tamanhoObjetos, valorPosicaoRato, valorPosicaoQueijo, inicioParedes);
 			}
 
 			cout << "Digite:\n1 para gerar outra fase do mesmo tamanho.\n2 para escolher outro tamanho da fase.\n3 para sair do programa:";
